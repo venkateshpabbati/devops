762807f1875ae46155bfe21bf439fa9dbd1306a5
raogaru <raogaruinc@gmail.com>
2024-03-18 18:57:31 -0400

    Update pgtap_test6.sql

--- vagrant/postgres17/pgtap/pgtap_test6.sql
+++ vagrant/postgres17/pgtap/pgtap_test6.sql
@@ -7,10 +7,6 @@
 
 set search_path to "$user", public, postgres;
 
-select concat('pg_version: ',pg_version());
-select concat('os_name: ',os_name());
-select concat('pgTAP version: ',pgtap_version());
-
 select 'TEST: Performance testing';
 
 select plan(5);
