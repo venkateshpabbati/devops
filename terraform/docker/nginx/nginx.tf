c0fce5e2f609c0680899511aa5c45075b6906c26
raogaru <raogaruinc@gmail.com>
2024-11-09 20:28:05 -0500

    Update nginx.tf

--- terraform/docker/nginx/nginx.tf
+++ terraform/docker/nginx/nginx.tf
@@ -16,11 +16,11 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "tutorial"
+  name  = "nginx"
 
   ports {
     internal = 80
-    external = 8000
+    external = 8051
   }
 }
 
